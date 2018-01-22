4579ccf39df24fc4159c287e6b26f10ad407c1f1
Jason <1065220365@qq.com>
2018-01-22 20:34:31 +0800

    hbs

---
+++ src/js/components/likeComponent/list.js
@@ -0,0 +1,7 @@
+module.exports = function (items, options) {
+    var temp = '<ul>';
+    for (var i = 0; i < items.length; i++) {
+        temp += '<li>' + options.fn(items[i]) + '</li>';
+    }
+    return temp + '</ul>';
+};
